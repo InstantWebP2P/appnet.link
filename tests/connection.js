a9aafb23ff70b180734e201dc2d89f2516e286c7
tom zhou <zs68j2ee@gmail.com>
2013-01-04 21:44:57 +0800

    added name-server connection test case

---
+++ tests/connection.js
@@ -0,0 +1,29 @@
+
+// p2p stream websocket library
+var WebSocket = require('wspp');
+
+// connecting to primary name-server
+var con = new WebSocket('ws://iwebpp.com:51686');
+
+var t = setTimeout(function(){
+    console.log('connecting to primary name-server timeout');
+}, 2000); // 2s in default
+
+con.on('open', function(){
+    clearTimeout(t);
+    console.log('connecting to primary name-server successfully');
+    con.close();
+});
+
+// connecting to alternative name-server
+var con1 = new WebSocket('ws://iwebpp.com:51868');
+
+var t1 = setTimeout(function(){
+    console.log('connecting to alternative name-server timeout');
+}, 2000); // 2s in default
+
+con1.on('open', function(){
+    clearTimeout(t1);
+    console.log('connecting to alternative name-server successfully');
+    con1.close();
+});
