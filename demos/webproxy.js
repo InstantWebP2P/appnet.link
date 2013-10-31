8df55cd831c1cc63bec3d627e59c3cff4a452c8b
tom zhou <zs68j2ee@gmail.com>
2013-10-31 08:52:28 +0800

    take forward proxy to separate module,leave stun proxy

--- demos/webproxy.js
+++ demos/webproxy.js
@@ -26,22 +26,12 @@ nmcln.on('ready', function(){
     
     // start web proxy service
     var https = require('https');
-    var http = require('http');
-    var webpxySrv = https.createServer(self.secerts);
-    var webpxySrv1 = http.createServer();
+    var srv = https.createServer(self.secerts);
     
-    var reverse = self.webProxy({https: true});
-    webpxySrv.on('request', reverse.httpApp); 
-    webpxySrv.on('upgrade', reverse.wsApp);
+    var proxy = self.webProxy({https: true});
+    srv.on('request', proxy.httpApp); 
+    srv.on('upgrade', proxy.wsApp);
     
-    webpxySrv.listen(51688);
-    console.log('web reverse proxy server listen on port 51688');
-    
-    var forward = self.webProxy({https: false, forward: true});
-    webpxySrv1.on('request', forward.httpApp); 
-    webpxySrv1.on('upgrade', forward.wsApp);
-    webpxySrv1.on('connect', forward.tunnelApp);
-    
-    webpxySrv1.listen(51866);
-    console.log('web forwar proxy server listen on port 51866');
+    srv.listen(51688);
+    console.log('Web proxy server listen on port 51688');
 });
